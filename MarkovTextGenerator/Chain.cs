56ef198d82903cce62b8281bf5168480513291fe
tanczosm <mike@logic-gate.com>
2017-11-28 09:12:32 -0500

    Initial Commit

---
+++ MarkovTextGenerator/Chain.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovTextGenerator
+{
+    class Chain
+    {
+        public Dictionary<String, List<Word>> words;
+        private Random rand;
+
+        public Chain ()
+        {
+            words = new Dictionary<String, List<Word>>();
+            rand = new Random(System.Environment.TickCount);
+        }
+
+        // This may not be the best approach.. better may be to actually store
+        // a separate list of actual sentence starting words and randomly choose from that
+        public String GetRandomStartingWord ()
+        {
+            return words.Keys.ElementAt(rand.Next() % words.Keys.Count);
+        }
+
+        // Adds a sentence to the chain
+        // You can use the empty string to indicate the sentence will end
+        //
+        // For example, if sentence is "The house is on fire" you would do the following:
+        //  AddPair("The", "house")
+        //  AddPair("house", "is")
+        //  AddPair("is", "on")
+        //  AddPair("on", "fire")
+        //  AddPair("fire", "")
+
+        public void AddString (String sentence)
+        {
+            // TODO: Break sentence up into word pairs
+            // TODO: Add each word pair to the chain
+        }
+
+        // Adds a pair of words to the chain that will appear in order
+        public void AddPair(String word, String word2)
+        {
+            if (!words.ContainsKey(word))
+            {
+                words.Add(word, new List<Word>());
+                words[word].Add(new Word(word2));
+            }
+            else
+            {
+                foreach (Word s in words[word])
+                {
+                    if (s.ToString() == word2)
+                    {
+                        s.Count++;
+                    }
+                }
+            }
+        }
+
+        // Given a word, randomly chooses the next word
+        public String GetNextWord (String word)
+        {
+            if (words.ContainsKey(word))
+            {
+                double choice = 1.0 / (double)rand.Next(100000);
+
+                Console.WriteLine("I picked the number " + choice); 
+            }
+
+            return "idkbbq";
+        }
+
+        public void UpdateProbabilities ()
+        {
+            foreach (String word in words.Keys)
+            {
+                double sum = 0;  // Total sum of all the occurences of each followup word
+
+                // Step 1:  Get the sum of all occurences of each followup word
+                foreach (Word s in words[word])
+                {
+                    // TODO: Finish me
+                }
+
+                // Step 2:  Update the probabilities
+                foreach (Word s in words[word])
+                {
+                    // TODO: Finish me
+                }
+
+            }
+        }
+    }
+}
---
+++ MarkovTextGenerator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovTextGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chain chain = new Chain();
+
+            Console.WriteLine("Welcome to Marky Markov's Random Text Generator!");
+
+            Console.WriteLine("Enter some text I can learn from (enter single ! to finish): ");
+
+            while (true)
+            {
+
+                Console.Write("> ");
+
+                String line = Console.ReadLine();
+                if (line == "!")
+                    break;
+
+                chain.AddString(line);  // Let the chain process this string
+            }
+
+            // Now let's update all the probabilities with the new data
+            chain.UpdateProbabilities();
+
+            // Okay now for the fun part
+            Console.WriteLine("Done learning!  Now give me a word and I'll tell you what comes next.");
+            Console.Write("> ");
+
+            String word = Console.ReadLine();
+            String nextWord = chain.GetNextWord(word);
+            Console.WriteLine("I predict the next word will be " + nextWord);
+        }
+    }
+}
